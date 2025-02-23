4fbf4f2f4a5233b4bdb1952fdf3031621289557b
Heyner Maluendas <maloheynerandres@gmail.com>
2025-02-23 12:19:29 -0500

    Corrigiendo configuración para Railway

--- src/config.ts
+++ src/config.ts
@@ -1,6 +1,6 @@
 const config = {
   BaseUrlWS: 'ws://127.0.0.1:8000',
-  BaseUrl:'http://localhost:8000',
+  BaseUrl:'https://tus-megas-sas-bakend-production.up.railway.app',
 };
 
 export default config;
