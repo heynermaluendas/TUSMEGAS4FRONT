6e092f2a1534d312d32927dc49c37b6c6a0b7ecc
Heyner Maluendas <maloheynerandres@gmail.com>
2025-02-24 15:43:47 -0500

    ant pro layauts

--- src/config.ts
+++ src/config.ts
@@ -1,6 +1,7 @@
 const config = {
   BaseUrlWS: 'ws://127.0.0.1:8000',
   BaseUrl:'https://tus-megas-sas-bakend-production.up.railway.app',
+  // BaseUrl:'http://localhost:3000',
 };
 
 export default config;
