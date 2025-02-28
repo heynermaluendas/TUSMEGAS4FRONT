cca4770151563eb85839327b7f670aa86928fef0
Heyner <maloheynerandres@gmail.com>
2025-02-28 10:49:55 -0500

    bugs mini

--- src/pages/Welcome.tsx
+++ src/pages/Welcome.tsx
@@ -1,17 +1,48 @@
-import { useState, useEffect } from "react";
-import { Table, Spin, Alert, Input, Button,notification ,message } from "antd";
-import axios from "axios";
-import config from "../config"; 
+import { useState, useEffect } from 'react';
+import { Table, Spin, Alert, Input, Button, message } from 'antd';
+import axios from 'axios';
+import config from '../config';
 
-const palabrasFiltradas = ["VACIO", "RESPALDO","REPALDO","POWER", "QUEUE", "ANTENA","PLAN","LAP","17.",]; // Palabras a ocultar por defecto
+const palabrasFiltradas = [
+  'VACIO',
+  'RESPALDO',
+  'REPALDO',
+  'POWER',
+  'QUEUE',
+  'ANTENA',
+  'PLAN',
+  'LAP',
+  '17.',
+]; // Palabras a ocultar por defecto
 
 const UsuariosSinId = () => {
   const [usuarios, setUsuarios] = useState([]);
   const [filteredUsuarios, setFilteredUsuarios] = useState([]);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
-  const [searchText, setSearchText] = useState("");
+  const [searchText, setSearchText] = useState('');
   const [filtroActivo, setFiltroActivo] = useState(true); // Estado para activar/desactivar el filtro
+
+  const aplicarFiltro = (data, busqueda, filtrar) => {
+    let resultado = data.filter(
+      (usuario) =>
+        usuario['.id'].toLowerCase().includes(busqueda.toLowerCase()) ||
+        usuario.name.toLowerCase().includes(busqueda.toLowerCase()),
+    );
+
+    if (filtrar) {
+      resultado = resultado.filter(
+        (usuario) =>
+          !palabrasFiltradas.some(
+            (palabra) =>
+              usuario.name.toUpperCase().includes(palabra) ||
+              usuario['.id'].toUpperCase().includes(palabra),
+          ),
+      );
+    }
+
+    setFilteredUsuarios(resultado);
+  };
 
   useEffect(() => {
     const fetchData = async () => {
@@ -19,17 +50,25 @@ const UsuariosSinId = () => {
         const responseUsuarios = await axios.get(`${config.BaseUrl}/mikrotik/usuarios/`);
         const responseContratantes = await axios.get(`${config.BaseUrl}/contratantes/`);
 
-        const usuariosData = Array.isArray(responseUsuarios.data.usuarios) ? responseUsuarios.data.usuarios : [];
-        const contratantesData = Array.isArray(responseContratantes.data) ? responseContratantes.data : responseContratantes.data.data || [];
+        const usuariosData = Array.isArray(responseUsuarios.data.usuarios)
+          ? responseUsuarios.data.usuarios
+          : [];
+        const contratantesData = Array.isArray(responseContratantes.data)
+          ? responseContratantes.data
+          : responseContratantes.data.data || [];
 
-        const idsContratantes = new Set(contratantesData.map((c) => c.plan_contratado_mes_atrasado).filter(Boolean));
+        const idsContratantes = new Set(
+          contratantesData.map((c) => c.plan_contratado_mes_atrasado).filter(Boolean),
+        );
 
-        const usuariosSinId = usuariosData.filter((usuario) => !idsContratantes.has(usuario[".id"]));
+        const usuariosSinId = usuariosData.filter(
+          (usuario) => !idsContratantes.has(usuario['.id']),
+        );
 
         setUsuarios(usuariosSinId);
         aplicarFiltro(usuariosSinId, searchText, filtroActivo); // Aplicar filtro al cargar
       } catch (err) {
-        console.error("Error al cargar datos:", err);
+        console.error('Error al cargar datos:', err);
         setError(`Error al cargar los datos: ${err.message}`);
       } finally {
         setLoading(false);
@@ -38,20 +77,6 @@ const UsuariosSinId = () => {
 
     fetchData();
   }, []);
-
-  const aplicarFiltro = (data, busqueda, filtrar) => {
-    let resultado = data.filter((usuario) =>
-      usuario[".id"].toLowerCase().includes(busqueda.toLowerCase()) || usuario.name.toLowerCase().includes(busqueda.toLowerCase())
-    );
-
-    if (filtrar) {
-      resultado = resultado.filter((usuario) =>
-        !palabrasFiltradas.some((palabra) => usuario.name.toUpperCase().includes(palabra) || usuario[".id"].toUpperCase().includes(palabra))
-      );
-    }
-
-    setFilteredUsuarios(resultado);
-  };
 
   const handleSearch = (e) => {
     const value = e.target.value.toLowerCase();
@@ -64,25 +89,27 @@ const UsuariosSinId = () => {
     aplicarFiltro(usuarios, searchText, !filtroActivo);
   };
   const copyToClipboard = (id) => {
-    navigator.clipboard.writeText(id).then(() => {
-      message.success("Copiado al portapapeles");
-    }).catch(() => {
-      message.error("Error al copiar");
-    });
+    navigator.clipboard
+      .writeText(id)
+      .then(() => {
+        message.success('Copiado al portapapeles');
+      })
+      .catch(() => {
+        message.error('Error al copiar');
+      });
   };
-  
 
   const columns = [
     {
-      title: "ID MikroTik",
-      dataIndex: ".id",
-      key: ".id",
+      title: 'ID MikroTik',
+      dataIndex: '.id',
+      key: '.id',
       render: (id) => (
-        <div style={{ display: "flex", alignItems: "center" }}>
+        <div style={{ display: 'flex', alignItems: 'center' }}>
           <span>{id}</span>
           <Button
             size="small"
-            style={{ marginLeft: "30%"  }}
+            style={{ marginLeft: '30%' }}
             onClick={() => copyToClipboard(id)}
             type="primary"
           >
@@ -91,9 +118,8 @@ const UsuariosSinId = () => {
         </div>
       ),
     },
-    { title: "Usuario", dataIndex: "name", key: "name" },
+    { title: 'Usuario', dataIndex: 'name', key: 'name' },
   ];
-  
 
   return (
     <div>
@@ -101,11 +127,11 @@ const UsuariosSinId = () => {
         placeholder="Buscar por ID o Nombre..."
         value={searchText}
         onChange={handleSearch}
-        style={{ marginBottom: 10, minWidth: "50%" }}
+        style={{ marginBottom: 10, minWidth: '60%' }}
       />
 
       <Button onClick={toggleFiltro} style={{ marginBottom: 10 }}>
-        {filtroActivo ? "Desactivar Filtro" : "Activar Filtro"}
+        {filtroActivo ? 'Desactivar Filtro' : 'Activar Filtro'}
       </Button>
 
       {error && <Alert message={error} type="error" />}
@@ -120,7 +146,7 @@ const UsuariosSinId = () => {
           components={{
             body: {
               cell: ({ children }) => (
-                <td style={{ padding: "4px", fontSize: "12px" }}>{children}</td>
+                <td style={{ padding: '4px', fontSize: '12px' }}>{children}</td>
               ),
             },
           }}
